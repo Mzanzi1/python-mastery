be93aaa1825ec15c67377a279e4622ba88f3bed2
Gordon McArthur <61467527+Mzanzi1@users.noreply.github.com>
2026-03-11 15:39:53 +0400

    Week 1: variables and basic operations

---
+++ month1-basics/week1/practice.py
@@ -0,0 +1,23 @@
+# Week 1 - Lesson 1
+
+# Variables
+name = "Gordon"
+goal = "Data Analysis and Automation"
+hours_per_week = 3
+print("My name is", name)
+print("My goal is", goal)
+print("I can study", hours_per_week, "hours per week")
+
+# Math operations
+a = 15
+b = 4
+
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+
+# Challenge
+years = 10
+total_study_hours = years * 52 * hours_per_week
+print("If I stay consistent, I will study", total_study_hours, "hours in 5 years.")
